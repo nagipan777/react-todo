fa21dd82e42e186c3a897f6ecf49fe7e68898534
nagipan777 <nagipan777@gmail.com>
2023-06-05 01:33:28 +0900

    auth

--- src/hooks/useMutateAuth.ts
+++ src/hooks/useMutateAuth.ts
@@ -6,53 +6,53 @@ import { Credential } from '../types'
 import { useError } from '../hooks/useError'
 
 export const useMutateAuth = () => {
-    const navigate = useNavigate()
-    const resetEditedTask = useStore((state) => state.resetEditedTask)
-    const { switchErrorHandling } = useError()
-    const loginMutation = useMutation(
-        async (user: Credential) =>
-          await axios.post(`${process.env.REACT_APP_API_URL}/login`, user),
-        {
-          onSuccess: () => {
-            navigate('/todo')
-          },
-          onError: (err: any) => {
-            if (err.response.data.message) {
-              switchErrorHandling(err.response.data.message)
-            } else {
-              switchErrorHandling(err.response.data)
-            }
-          },
+  const navigate = useNavigate()
+  const resetEditedTask = useStore((state) => state.resetEditedTask)
+  const { switchErrorHandling } = useError()
+  const loginMutation = useMutation(
+    async (user: Credential) =>
+      await axios.post(`${process.env.REACT_APP_API_URL}/login`, user),
+    {
+      onSuccess: () => {
+        navigate('/todo')
+      },
+      onError: (err: any) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
         }
-      )
-      const registerMutation = useMutation(
-        async (user: Credential) =>
-          await axios.post(`${process.env.REACT_APP_API_URL}/signup`, user),
-        {
-          onError: (err: any) => {
-            if (err.response.data.message) {
-              switchErrorHandling(err.response.data.message)
-            } else {
-              switchErrorHandling(err.response.data)
-            }
-          },
+      },
+    }
+  )
+  const registerMutation = useMutation(
+    async (user: Credential) =>
+      await axios.post(`${process.env.REACT_APP_API_URL}/signup`, user),
+    {
+      onError: (err: any) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
         }
-      )
-      const logoutMutation = useMutation(
-        async () => await axios.post(`${process.env.REACT_APP_API_URL}/logout`),
-        {
-          onSuccess: () => {
-            resetEditedTask()
-            navigate('/')
-          },
-          onError: (err: any) => {
-            if (err.response.data.message) {
-              switchErrorHandling(err.response.data.message)
-            } else {
-              switchErrorHandling(err.response.data)
-            }
-          },
+      },
+    }
+  )
+  const logoutMutation = useMutation(
+    async () => await axios.post(`${process.env.REACT_APP_API_URL}/logout`),
+    {
+      onSuccess: () => {
+        resetEditedTask()
+        navigate('/')
+      },
+      onError: (err: any) => {
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
         }
-      )
-      return { loginMutation, registerMutation, logoutMutation }
+      },
+    }
+  )
+  return { loginMutation, registerMutation, logoutMutation }
 }
