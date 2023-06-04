cc1123fb74b08c642d39f02e67ee10ecf6742528
nagipan777 <nagipan777@gmail.com>
2023-06-04 22:27:42 +0900

    app

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,24 @@ import axios from 'axios'
 import { CsrfToken } from './types'
 
 function App() {
-useEffect(() => {
-  axios.defaults.withCredentials = true
-  const getCsrfToken = async () => {
-    const { data } = await axios.get<CsrfToken>(
-      `${process.env.REACT_APP_API_URL}/csrf`
-    )
-    axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
-  }
-  console.log(getCsrfToken());
-  getCsrfToken()
-}, [])
-return (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Auth />} />
-      <Route path="/todo" element={<Todo />} />
-    </Routes>
-  </BrowserRouter>
-)
+  useEffect(() => {
+    axios.defaults.withCredentials = true
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`
+      )
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
